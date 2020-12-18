7852a7b3a357a1eddf1cf1c0d192ff7924f587ad
SymphonySimper <srisenthilbalaji2@gmail.com>
2020-12-18 14:39:52 +0530

    colors are almost perfect

--- config.def.h
+++ config.def.h
@@ -9,14 +9,15 @@ static const char *fonts[] = {"poppins:size=10"};
 static const char dmenufont[] = "poppins:size=10";
 
 static const char col_gray1[] = "#000000";
+static const char col_gray5[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
 static const char col_cyan[] = "#ffffff";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = {col_gray3, col_gray1, col_gray2},
-	[SchemeSel] = {col_gray4, col_gray1, col_gray3},
+	[SchemeNorm] = {col_gray3, col_gray1, col_gray5},
+	[SchemeSel] = {col_gray4, col_gray1, col_gray2},
 };
 
 static const char *const autostart[] = {
