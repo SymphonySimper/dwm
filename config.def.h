e9f6e65b4c04e7288597d77ea5232ed41c508a53
SymphonySimper <srisenthilbalaji2@gmail.com>
2021-03-05 16:02:49 +0530

    small

--- config.def.h
+++ config.def.h
@@ -107,7 +107,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray3, "-sf", col_gray5, NULL};
 static const char *termcmd[] = {"alacritty", NULL};
 static const char *firefoxcmd[] = {"firefox", NULL};
-static const char *sptcmd[] = {"alacritty", "-e", "/home/s2b/scripts/spotifydrun.sh", NULL};
+static const char *sptcmd[] = {"alacritty", "-e", "bash", "/home/s2b/scripts/spotifydrun.sh", NULL};
 static const char *gamemodecmd[] = {"/home/s2b/scripts/gameMode.sh", NULL};
 
 //SHCMD
