c12198039d9c750003a3e7b72aae32fbad70ae18
SymphonySimper <symphonysimper@gamil.com>
2021-06-03 14:03:59 +0530

    tag 6 glyph changed to figma logo and added figma-linux tagmask

--- config.def.h
+++ config.def.h
@@ -60,7 +60,7 @@ static const char *const autostart[] = {
 // tagging
 // static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
-static const char *tags[] = {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "};
+static const char *tags[] = {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -76,6 +76,7 @@ static const Rule rules[] = {
 	{"mpv", NULL, NULL, 1 << 3, 0, -1},
 	{"discord", NULL, NULL, 1 << 4, 0, -1},
 	{"lightcord", NULL, NULL, 1 << 4, 0, -1},
+	{"figma-linux",NULL, NULL, 1 << 5, 0, -1},
 	{"Spotify", NULL, NULL, 1 << 5, 0, -1},
 	{"whatsapp-nativefier", NULL, NULL, 1 << 5, 0,-1},
 	{"Pcmanfm", NULL, NULL, 1 << 6, 0, -1},
