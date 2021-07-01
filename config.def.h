7c0279810734ff8bcdb4e8a6c9f09671b6e8d2b9
SymphonySimper <symphonysimper@gamil.com>
2021-07-02 00:12:53 +0530

    switched from alacritty to kitty

--- config.def.h
+++ config.def.h
@@ -34,9 +34,9 @@ typedef struct
 	const void *cmd;
 
 } Sp;
-const char *spcmd1[] = {"alacritty", "-t", "spt0", "-e", "pulsemixer", "--color", "0", NULL};
-const char *spcmd2[] = {"alacritty", "-t", "spt1", NULL};
-const char *spcmd3[] = {"alacritty", "-t", "spm","-e", "bash", "spotifydrun", NULL};
+const char *spcmd1[] = {"kitty", "-T","spt0", "pulsemixer", "--color", "0", NULL};
+const char *spcmd2[] = {"kitty", "-T", "spt1", NULL};
+const char *spcmd3[] = {"kitty", "-T", "spm", "spotifydrun", NULL};
 
 static Sp scratchpads[] = {
 	// name          cmd
@@ -142,7 +142,7 @@ static const Layout layouts[] = {
 // commands
 static char dmenumon[2] = "0"; // component of dmenucmd, manipulated in spawn()
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray3, "-sf", col_gray5, NULL};
-static const char *termcmd[] = {"alacritty", NULL};
+static const char *termcmd[] = {"kitty", NULL};
 static const char *firefoxcmd[] = {"firefox", NULL};
 static const char *gamemodecmd[] = {"gameMode", NULL};
 static const char *searchcmd[] = {"search", NULL};
